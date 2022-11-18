4b174361d3f0446fad3ca3eccb6134e783ee8e8a
Igor <avatar200297@gmail.com>
2022-11-19 01:08:18 +0300

    Add new page

---
+++ NeuroAnalyzer/Pages/PsyhoPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NeuroAnalyzer.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PsyhoPage.xaml
+    /// </summary>
+    public partial class PsyhoPage : Page
+    {
+        public PsyhoPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Back_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MainPage());
+        }
+
+        private void Button_Start_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
