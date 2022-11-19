4d480bd08c238b42adee41b3ddc0ab94b2ae3630
Алексей <zimenkov71rus.alex@mail.ru>
2022-11-19 10:52:55 +0300

    Added Tree images and Pages

---
+++ NeuroAnalyzer/Pages/TreePages/TreePageGame.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace NeuroAnalyzer.Pages.TreePages
+{
+    /// <summary>
+    /// Логика взаимодействия для TreePageGame.xaml
+    /// </summary>
+    public partial class TreePageGame : Page
+    {
+        public TreePageGame()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Back_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MainPage());
+        }
+
+        private void Button_Start_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
