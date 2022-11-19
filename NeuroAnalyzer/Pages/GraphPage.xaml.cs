5f5e17ebfd241a767cf03150a06ff9f241dbc550
Виктор Яковлев <yava.victor.yava@gmail.com>
2022-11-19 14:46:28 +0300

    Update 2

--- NeuroAnalyzer/Pages/GraphPage.xaml.cs
+++ NeuroAnalyzer/Pages/GraphPage.xaml.cs
@@ -21,11 +21,18 @@ namespace NeuroAnalyzer.Pages
         private double _graphWidth, _graphHeight;
         
         public ChartValues<MeasureModel> Values { get; set; }
+        public ChartValues<int> Values1 { get; set; }
+        public ChartValues<int> Values2 { get; set; }
 
         public GraphPage()
         {
             InitializeComponent();
-            
+
+            Loaded += GraphLoaded;
+
+            Values1 = new ChartValues<int>();
+            Values2 = new ChartValues<int>();
+
             var mapper = Mappers.Xy<MeasureModel>()
                 .X(x => x.ElapsedMilliseconds)
                 .Y(x => x.Value);
@@ -41,7 +48,7 @@ namespace NeuroAnalyzer.Pages
             {
                 while (true)
                 {
-                    Thread.Sleep(1);
+                    Thread.Sleep(10);
 
                     //we add the lecture based on our StopWatch instance
                     Values.Add(new MeasureModel
@@ -49,7 +56,19 @@ namespace NeuroAnalyzer.Pages
                         ElapsedMilliseconds = sw.ElapsedMilliseconds,
                         Value = SerialInterfaceClass.graphPoint
                     });
-                    
+
+                    Values1.Clear();
+                    Values2.Clear();
+                    for (int i = 0; i < 32; i++)
+                    {
+                        Values1.Add(spectrumData[i]);
+                    }
+
+                    for (int i = 5; i < 10; i++)
+                    {
+                        Values2.Add(spectrumData[i] * 5);
+                    }
+
                     if (Values.Count > 200) Values.RemoveAt(0);
                 }
             });
@@ -61,40 +80,56 @@ namespace NeuroAnalyzer.Pages
         {
             graphUpdateTimer = new Timer(RefreshGraph, new AutoResetEvent(false), 0, 10);
         }
+        int[] spectrumData = SerialInterfaceClass.GetSpectrumData();
 
         private void RefreshGraph(object? obj)
         {
-            Dispatcher.Invoke(() =>
+            Values1.Clear();
+            Values2.Clear();
+            for (int i = 0; i < 32; i++)
             {
-                StackPanel stackPanel = new()
-                {
-                    Orientation = Orientation.Horizontal
-                };
-                SpectrumGrid.Children.Clear();
-                Rectangle graphBounds = new()
-                {
-                    StrokeThickness = 1,
-                    Stroke = Brushes.Black
-                };
-                SpectrumGrid.Children.Add(graphBounds);
+                Values1.Add(spectrumData[i]);
+            }
 
-                int[] spectrumData = SerialInterfaceClass.GetSpectrumData();
-                for (int i = 0; i < 32; i++)
-                {
-                    stackPanel.Children.Add(new Rectangle()
-                    {
-                        Width = _graphWidth / 32,
-                        Margin = new Thickness(0,0,0,1),
-                        Height = Math.Min(spectrumData[i], _graphHeight),
-                        Fill = Brushes.Aquamarine,
-                        StrokeThickness = 0,
-                        VerticalAlignment = VerticalAlignment.Bottom,
-                        HorizontalAlignment = HorizontalAlignment.Center
-                    });
-                }
-                SpectrumGrid.Children.Add(stackPanel);
-            });
+            for (int i = 5; i < 10; i++)
+            {
+                Values2.Add(spectrumData[i] * 5);
+            }
         }
+
+        //private void RefreshGraph(object? obj)
+        //{
+        //    Dispatcher.Invoke(() =>
+        //    {
+        //        StackPanel stackPanel = new()
+        //        {
+        //            Orientation = Orientation.Horizontal
+        //        };
+        //        SpectrumGrid.Children.Clear();
+        //        Rectangle graphBounds = new()
+        //        {
+        //            StrokeThickness = 1,
+        //            Stroke = Brushes.Black
+        //        };
+        //        SpectrumGrid.Children.Add(graphBounds);
+
+        //        int[] spectrumData = SerialInterfaceClass.GetSpectrumData();
+        //        for (int i = 0; i < 32; i++)
+        //        {
+        //            stackPanel.Children.Add(new Rectangle()
+        //            {
+        //                Width = _graphWidth / 32,
+        //                Margin = new Thickness(0,0,0,1),
+        //                Height = Math.Min(spectrumData[i], _graphHeight),
+        //                Fill = Brushes.Aquamarine,
+        //                StrokeThickness = 0,
+        //                VerticalAlignment = VerticalAlignment.Bottom,
+        //                HorizontalAlignment = HorizontalAlignment.Center
+        //            });
+        //        }
+        //        SpectrumGrid.Children.Add(stackPanel);
+        //    });
+        //}
 
         private void GridSizeChanged(object sender, RoutedEventArgs e)
         {
